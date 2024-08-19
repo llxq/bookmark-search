875df508963e904208e60324efb657ebf9c235c0
chendf <1725760203@qq.com>
2024-08-19 09:45:25 +0800

    修改通过扩展点击打开的页面

--- popup.js
+++ popup.js
@@ -87,9 +87,16 @@ document.getElementById('bookmarksSearchList').addEventListener('click', goToBoo
 const container = document.getElementById('bookmarksSearchContainer')
 if (container) {
     Object.assign(container.style, {
-        width: isIframe ? '100%' : '500px',
-        height: isIframe ? '100%' : '500px',
-    }) 
+        width: isIframe ? '100%' : '300px',
+        height: isIframe ? '100%' : 'max-content',
+    })
+    if (!isIframe) {
+        container.classList.add('tips__container')
+        container.innerHTML = `这是一个chroma插件，可以按住 ctrl/command + B 进行搜索您的书签，并且点击打开您的书签。支持上下切换回车打开新页面。支持文件夹搜索。多层级文件夹使用 / 分割。如：文件夹1/文件夹2/标签1。有问题欢<a id="feedback" href="https://github.com/llxq/bookmark-search/issues/new">反馈</a>。`
+        document.getElementById('feedback').onclick = () => {
+            window.open('https://github.com/llxq/bookmark-search/issues/new')
+        }
+    }
 }
 
 chrome.runtime?.onMessage?.addListener(message => {
